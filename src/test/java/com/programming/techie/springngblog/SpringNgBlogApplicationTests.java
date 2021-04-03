046a1b15c4cfffb705340dc59a7bd2a1f6c8fd3f
user <user@WINDOWS-VK4C0HF>
2021-04-03 16:48:51 +0200

    Initial commit.

---
+++ src/test/java/com/programming/techie/springngblog/SpringNgBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.programming.techie.springngblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNgBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
