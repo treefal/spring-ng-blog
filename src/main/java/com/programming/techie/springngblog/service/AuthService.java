fe87a97bf5a346dcb9c505145edd3bfb26ca0f43
user <user@WINDOWS-VK4C0HF>
2021-04-11 01:33:48 +0200

    Implement Authentication Response class.

--- src/main/java/com/programming/techie/springngblog/service/AuthService.java
+++ src/main/java/com/programming/techie/springngblog/service/AuthService.java
@@ -43,13 +43,16 @@ public class AuthService {
         return passwordEncoder.encode(password);
     }
 
-    public String login(LoginRequest loginRequest) {
+    public AuthenticationResponse login(LoginRequest loginRequest) {
         Authentication authenticate = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(
                         loginRequest.getUsername(),
                         loginRequest.getPassword()));
+
         SecurityContextHolder.getContext().setAuthentication(authenticate);
-        return jwtProvider.generateToken(authenticate);
+        String authenticationToken = jwtProvider.generateToken(authenticate);
+
+        return new AuthenticationResponse(authenticationToken, loginRequest.getUsername());
     }
 
     public Optional<org.springframework.security.core.userdetails.User> getCurrentUser() {
